28359fea0caa8a9c2889b6a00a931d6de0f9477e
Lawrence Tulod <118673700+intrep11d@users.noreply.github.com>
2024-10-06 17:43:59 +0800

    Update profileController.js

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,6 +3,6 @@ exports.getProfile = (req, res) => {
     res.json({
       id: req.user.id,
       username: req.user.username,
-      email: req.user.email, // You might need to fetch this from your data store
+      email: req.user.email, 
     });
   };
